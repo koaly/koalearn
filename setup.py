561784e9725747a5ee182375eead755cbd9ab421
koaly <koaly@koaly.me>
2019-06-28 14:45:07 +0700

    update setup.py and change name

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='koalearn',
+    version='0.0.1',
+    description='My private package for learning',
+    url='https://github.com/koaly/koalearn.git',
+    author='Natthapong Somboonphattarakit',
+    author_email='koaly@koaly.me',
+    license="Wisesight",
+    packages=find_packages(),
+    zip_safe=False
+)
